2ef0d7040da9b8f96824b9843c878391026251d5
karlpederolesen <96655854+karlpederolesen@users.noreply.github.com>
2021-12-27 20:16:43 +0100

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(antal)
+})
+input.onButtonPressed(Button.B, function () {
+    antal = 0
+})
+let antal = 0
+antal = 0
 basic.forever(function () {
-	
+    if (input.lightLevel() < 40) {
+        antal += 1
+        while (input.lightLevel() < 40) {
+        	
+        }
+    }
 })
